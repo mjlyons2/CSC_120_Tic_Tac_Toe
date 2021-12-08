9d991324e073fbe2c415f8a34b922aaaae91f9dd
Micah <mlyons2@my.waketech.edu>
2021-12-08 12:04:52 -0500

    Adding board.py

---
+++ board.py
@@ -0,0 +1,9 @@
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-", ]
+
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
